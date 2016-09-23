fca493ed0f971a3c55a23f2a519bc1b9e746f817
jacksonbates <malgalin@gmail.com>
2016-09-23 22:54:39 +1000

    implements ajax post request and fakes value on sub button

---
+++ public/scripts/ajax.js
@@ -0,0 +1,23 @@
+$('form').submit(function(event) {
+  event.preventDefault();
+  var $form = $( this ),
+    userEmail = $form.find( "input[name='userEmail']" ).val(),
+    location = $form.find( "input[name='location']" ).val(),
+    yelpId = $form.find( "input[name='yelpId']" ).val();
+  var subButton = $form.find( "input[type='submit']" );
+  var goingNumber = +subButton.val().match(/^\d*/)[0];
+  // subButton.val('RSVPing');
+  var userAttends = $form.find( "input[name='userAttendingVenue']" );
+  if (userAttends.val() === 'false') {
+    subButton.val((goingNumber + 1) + ' Going');
+    userAttends.val('true');
+  } else {
+    subButton.val((goingNumber - 1) + " Going");
+    userAttends.val('false');
+  }
+  console.log('attempting to POST', {userEmail: userEmail, location: location, yelpId: yelpId});
+  $.post('/process', { userEmail: userEmail, location: location, yelpId: yelpId })
+  .done(function( data ) {
+    console.log('done');  
+  });
+})
